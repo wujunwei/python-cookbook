0bb88450445a16f5d07b40e87d2842a663ce5764
adam123 <wjw3323@live.com>
2016-09-05 13:05:27 +0800

    添加hotkey

--- api/CookBook.py
+++ api/CookBook.py
@@ -7,6 +7,7 @@ import io
 import requests
 from PIL import Image
 from PIL import ImageTk
+from bs4 import BeautifulSoup
 
 
 class CookBook(object):
@@ -14,7 +15,7 @@ class CookBook(object):
     __url = "http://www.tngou.net/api/cook/"
     __classify = {'美容': 1, "减肥": 10, "保健养生": 15, "人群": 52}
     __ImagePrefix = (r"http://tnfs.tngou.net/image", r"http://tnfs.tngou.net/img")
-    __dataList = []
+    __dataList = {}
     __page = 1
     __image = None
     top_frame = None
@@ -29,7 +30,9 @@ class CookBook(object):
         self.root.resizable(True, True)
         self.center_window(800, 700)
         # 初始化变量
+        self.text = StringVar()
         self.selection = IntVar()
+        self.sel = IntVar()
         # 初始化frame
         self.init_frame()
         self.init_canvas()
@@ -37,36 +40,46 @@ class CookBook(object):
 
     def init_frame(self):
         self.init_top()
-        self.init_bottom()
         self.init_left()
+        self.init_bottom()
 
     def init_top(self):
         if self.top_frame is None:
-            self.top_frame = Frame(self.root, bg="yellow")
+            self.top_frame = Frame(self.root)
             self.top_frame.pack(side=TOP)
             i = 0
             for (key, value) in self.__classify.items():
-                Radiobutton(self.top_frame, indicatoron=0, variable=self.selection, text=key, value=value,
-                            command=lambda: self.change_list(1)).grid(row=0, column=i, sticky=W)
+                Radiobutton(self.top_frame, indicatoron=0, variable=self.selection, text=key, value=value, command=lambda: self.change_list(1)).grid(row=0, column=i, sticky=W)
                 i += 1
+        #
 
     def init_bottom(self):
         self.bottom_frame = Frame(self.root, bg="white")
         self.bottom_frame.pack(side=BOTTOM)
+        Label(self.bottom_frame, wraplength=400, text='请选择', textvariable=self.text).pack(side=RIGHT)
 
-    def init_canvas(self, img=None):
+    def init_canvas(self, sel_id=None):
         if self.canvas is None:
-            self.canvas = Canvas(self.root, bg="red")
+            self.canvas = Canvas(self.root, width=300, height=300)
         else:
             self.canvas.destroy()
-            self.canvas = Canvas(self.root, bg="red")
-        if img is not None:
-            img = self.get_img_url(img)
-            image_bytes = urlopen(img).read()
-            data_stream = io.BytesIO(image_bytes)
-            pil_image = Image.open(data_stream)
-            self.__image = ImageTk.PhotoImage(pil_image)
-            self.canvas.create_image(100, 100, image=self.__image)
+            self.canvas = Canvas(self.root, width=300, height=300)
+        if sel_id is not None:
+            try:
+                detail = self.get_detail_json(sel_id)
+                bs = BeautifulSoup(detail['message'], "lxml")
+                temp = bs.find_all('p')
+                self.text.set("\n".join(t.string for t in temp))
+                img = detail['img']
+                img = self.get_img_url(img)
+                image_bytes = urlopen(img).read()
+                data_stream = io.BytesIO(image_bytes)
+                pil_image = Image.open(data_stream)
+                self.__image = ImageTk.PhotoImage(pil_image)
+                self.canvas.create_image(100, 50, image=self.__image)
+                # self.canvas.create_text(300, 450, text=text, fill='blue')
+            except Exception as e:
+                print(e)
         self.canvas.pack(side=RIGHT)
 
     def init_left(self):
@@ -79,28 +92,30 @@ class CookBook(object):
             self.left_frame.pack(side=LEFT)
         i = 0
         for var in self.__dataList:
-            Button(self.left_frame, bg="yellow", text=var['name'], command=lambda: self.init_canvas(), width=20).grid(
-                row=i, column=0, sticky=W)
+            Radiobutton(self.left_frame, indicatoron=0, variable=self.sel, bg="yellow", text=var['name'], value=var['id'],
+                        command=lambda: self.init_canvas(self.sel.get()), width=20).grid(row=i, column=0, sticky=W)
             i += 1
+
         if i != 0:
             Button(self.left_frame, bg="yellow", text="上一页", command=self.previous).grid(row=i, column=0, sticky=W)
             Button(self.left_frame, bg="yellow", text="下一页", command=self.next).grid(row=i, column=1, sticky=E)
 
     def next(self):
         self.__page += 1
-        self.change_list()
+        self.__dataList = self.get_list_json(self.selection.get(), self.__page)
+        self.init_left()
 
     def previous(self):
         self.__page -= 1
         if self.__page < 1:
             self.__page = 1
-        self.change_list()
+        self.__dataList = self.get_list_json(self.selection.get(), self.__page)
+        self.init_left()
 
     def change_list(self, page=None):
         if page is not None:
             self.__page = 1
-        json_data = self.get_list_json(self.selection.get(), self.__page)
-        self.__dataList = json_data['tngou']
+        self.__dataList = self.get_list_json(self.selection.get(), self.__page)
         self.init_left()
 
     def center_window(self, width, height):
@@ -112,7 +127,8 @@ class CookBook(object):
     def get_detail_json(self, select_id):
         try:
             result = requests.get(self.__url + "show", params={'id': select_id})
-            return result.text
+            obj = json.loads(result.text)
+            return obj
         except Exception as e:
             messagebox.askokcancel('error', '网络错误请保持网络通畅')
             print(e)
@@ -120,7 +136,7 @@ class CookBook(object):
     def get_list_json(self, classify_id, page=1, rows=20):
         result = requests.get(self.__url + "list", params={'id': classify_id, 'page': page, 'rows': rows})
         obj = json.loads(result.text)
-        return obj
+        return obj['tngou']
 
     def get_json_by_name(self, name):
         result = requests.get(self.__url + "name", params={'name': name})
